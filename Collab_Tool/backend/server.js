ee397da338af99b2e8ff612103367644e65746af
Ashitosh9922 <129257337+Ashitosh9922@users.noreply.github.com>
2025-01-05 10:28:27 +0530

    Update server.js

--- Collab_Tool/backend/server.js
+++ Collab_Tool/backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173', // Update with your frontend URL
+        origin: 'http://localhost:5173',
         methods: ['GET', 'POST'],
     },
 });
